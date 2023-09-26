a111f79c520d14240e5e3104dfbbdb12d05e1c3a
santanu0101 <144315013+santanu0101@users.noreply.github.com>
2023-09-26 12:49:09 +0000

    variable and dataType done

---
+++ js_for_begainers/Js_dataType.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+// alert( 3 + 3 ) //we are using nodeJs, Not Browser
+
+console.log(3 + 3);
+
+console.log("hero");
+
+
+let name = "hero"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => Standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
---
+++ js_for_begainers/Js_variable.js
@@ -0,0 +1,16 @@
+const accountId = 1236556
+let accountEmail = "san@gamil.com"
+var accountPassword = 123452
+accountCity = "Kolkata"
+let accountState
+
+
+
+// accountId = 122334 // This is a constance Not allowed
+accountEmail = "subham@gmail.com"
+accountCity = "bengaluru"
+
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
