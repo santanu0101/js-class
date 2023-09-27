0e507964ae587c375845c06e6bad0d934f08160b
santanu0101 <144315013+santanu0101@users.noreply.github.com>
2023-09-27 08:01:01 +0000

    conversion operation done

---
+++ js_for_begainers/Js_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = undefined
+let score1 = null
+let score2 = "33"
+let score3 = true
+
+
+console.log(typeof score)
+console.log(typeof score1)
+console.log(typeof score2)
+console.log(typeof score3)
+
+
+let ValueInNumber = Number(score)
+console.log(typeof ValueInNumber)
+console.log(ValueInNumber)
+
+let ValueInNumber1 = Number(score1)
+console.log(typeof ValueInNumber1)
+console.log(ValueInNumber1)
+
+let ValueInNumber2 = Number(score2)
+console.log(typeof ValueInNumber2)
+console.log(ValueInNumber2)
+
+let ValueInNumber3 = Number(score3)
+console.log(typeof ValueInNumber3)
+console.log(ValueInNumber3)
